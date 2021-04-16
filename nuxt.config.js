78e7f450e07e8976aea58d518d9d83c66f67f48c
Artem <artmyseo@yandex.ru>
2021-04-16 10:28:30 +0300

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'vse-lesa',
+    htmlAttrs: {
+      lang: 'ru'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { hid: 'stripe', src: '/core2.js', defer: false }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/css/bootstrap.css',
+    '@/assets/css/styles.css',
+    '@/assets/css/all.css',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    '@nuxtjs/style-resources',
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    plugins: [
+      new webpack.ProvidePlugin ({
+        '$': 'jquery',
+	'jQuery': 'jquery',
+        '_': 'lodash'
+      }),
+    ]
+  },
+  styleResources: {
+    less: [ '@/assets/less/*.less'],
+},
+}
