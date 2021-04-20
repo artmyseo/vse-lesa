f2723b92c5eb66af0fedd58f7810c0c037085029
Artem <artmyseo@yandex.ru>
2021-04-20 15:34:47 +0300

    first commit

---
+++ components/LoadingBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="loading" class="loading-page">
+    <div class="loading"></div>
+  </div>
+</template>
+<script>
+  export default {
+    data: () => ({
+      loading: false,
+    }),
+    methods: {
+      start() {
+        this.loading = true
+      },
+      finish() {
+        this.loading = false
+      },
+    },
+  }
+</script>
+<style scoped>
+  .loading-page {
+    position: fixed;
+	width:100%;
+    height: 100%;
+    z-index: 1000;
+    padding: 1rem;
+    text-align: center;
+    font-size: 3rem;
+    font-family: sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+	background: #fff;
+	opacity: .3;
+  }
+  .loading {
+	margin: 0 auto;  
+    display: inline-block;
+    width: 5rem;
+    height: 5rem;
+    border: 5px solid #363b43;
+    border-radius: 50%;
+    border-top-color: #fff;
+	opacity: 1;
+    animation: spin 1s ease-in-out infinite;
+  }
+  @keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+</style>
